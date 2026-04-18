093ffd12e6a861c71883096a2d840a1a06d99dbb
danielmq1606 <danielmarinquijada@gmail.com>
2026-04-18 12:06:49 -0400

    model app done

---
+++ newspaper/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    published_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
