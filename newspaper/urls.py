da1a5ac4326183e09b7bc925d2b4197d3ecd6d68
danielmq1606 <danielmarinquijada@gmail.com>
2026-04-25 11:37:18 -0400

    article detail done

--- newspaper/urls.py
+++ newspaper/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import ArticleListView
+from .views import ArticleListView, ArticleDetailView
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='article_list'),
+    path('articulos/<int:pk>/', ArticleDetailView.as_view(), name='article-detail'),
 ]
+
+# <int:pk> es un parámetro que se pasará a la vista para identificar el artículo específico que se desea mostrar.
+# int es de numero entero y pk es de primary key, es decir, el identificador único de cada artículo en la base de datos (clave primaria).
